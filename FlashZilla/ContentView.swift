462b82ab18e6ae3582da19a75e48da1c2555cee3
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-09-18 16:00:20 +0530

    Initial Commit

---
+++ FlashZilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Dhruv Bhardwaj on 18/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
